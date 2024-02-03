831d74892897950bcfc24525ad6abbf4f3b4926d
justinkropp <justinkropp@gmail.com>
2024-02-03 16:22:50 -0500

    adding Geist Mono font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      geist: ["Geist"],
+    },
   },
   plugins: [],
 }
-
